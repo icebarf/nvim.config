458464c81e78de69db5de01c65e92f108e08ad9a
icebarf <icy.amrit@gmail.com>
2025-11-28 18:03:09 +0530

    tabstops

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,11 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- tabs
+vim.o.shiftwdith = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
